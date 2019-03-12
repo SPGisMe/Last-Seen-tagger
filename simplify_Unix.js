e53fb166d4f02696dab4bf4b173dfa1a17da0956
Sean Gaston <46758834+SPGisMe@users.noreply.github.com>
2019-03-11 18:51:17 -0700

    Update simplify_Unix.js

--- simplify_Unix.js
+++ simplify_Unix.js
@@ -1,24 +1,17 @@
-var simplify = function(epochTimeUnix) {
+var simplify = function(epochTime) {
     let lastSeen = 'Last seen: ';
     let currentTime = Date.now()/1000;
-    let elapsed = currentTime - epochTimeUnix;
+    let elapsed = currentTime - epochTime;
     const toMins = mins => Math.floor(mins/60);
     const toHours = hours => Math.floor(toMins(hours)/60);
     const toDays = days => Math.floor(toHours(days)/24);
 
-    if (elapsed < 120) {
-        lastSeen += 'moments ago';
-    } else if (elapsed < 3600) {
-        lastSeen += toMins(elapsed) + ' minutes ago';
-    } else if (elapsed < 7000) {
-        lastSeen += '1 hour ago'
-    } else if (elapsed < 86400) {
-        lastSeen += toHours(elapsed) + ' hours ago';
-    } else if (elapsed < 172000) {
-        lastSeen += '1 day ago';
-    } else if (elapsed >  172000) {
-        lastSeen += toDays(elapsed) + ' days ago';
-    }
+    elapsed < 120 ? lastSeen += 'moments ago'
+    : elapsed < 3600 ? lastSeen += toMins(elapsed) + ' minutes ago'
+    : elapsed < 7000 ? lastSeen += '1 hour ago'
+    : elapsed < 86400 ? lastSeen += toHours(elapsed) + ' hours ago'
+    : elapsed < 172000 ? lastSeen += '1 day ago'
+    : lastSeen += toDays(elapsed) + ' days ago';
 
     return lastSeen;
 }
